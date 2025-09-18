2a7acf41077f6938e0e8cdf1a74529ac7256c008
abbasmunawaros313-oss <abbasmunawaros313@gmail.com>
2025-09-18 14:03:30 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMOTHc9U1whgepO8AAE0-xaK7xiNjqqVQ",
-  authDomain: "osportalmanagmnet.firebaseapp.com",
-  projectId: "osportalmanagmnet",
-  storageBucket: "osportalmanagmnet.firebasestorage.app",
-  messagingSenderId: "846394060555",
-  appId: "1:846394060555:web:a4c6d3b64af58cd77f3c0e"
+  apiKey: "AIzaSyCKCkfz6e5BZjZR3QLN7Eow7XM0nDuKr4U",
+  authDomain: "illiana-travels-jaweria.firebaseapp.com",
+  projectId: "illiana-travels-jaweria",
+  storageBucket: "illiana-travels-jaweria.firebasestorage.app",
+  messagingSenderId: "585483485041",
+  appId: "1:585483485041:web:3389049b30e7c0d7d500d0",
+  measurementId: "G-55739L3TS7"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
